273375ff600629c8f589df17ce1faa6d100add4c
Danilo Lustosa <danilolustosa@gmail.com>
2021-05-25 22:16:38 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { ServicoComponent } from './servico/servico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'servico', component: ServicoComponent},
+  {path:'contato', component: ContatoComponent},
+  {path:'', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicoComponent } from './servico/servico.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicoComponent,
+    ContatoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
